e3a1bfcf73767a5a81e69ee3676e2aaa709ff1a9
Lucy Monie <lucy.monie.hall@gmail.com>
2016-07-24 12:36:38 +0100

    updates info and tutorials; adds notes to script file

--- resources/info-and-tutorials.md
+++ resources/info-and-tutorials.md
@@ -4,3 +4,11 @@
 * You Might Not Need jQuery: http://youmightnotneedjquery.com/
 * Choosing Vanilla JavaScript: http://alistapart.com/blog/post/choosing-vanilla-javascript
 * Eloquent Javascript, Chapter 20 - Node: http://eloquentjavascript.net/20_node.html
+* ]CSS diner](http://flukeout.github.io/) for a walk through CSS selectors.
+
+LM: I got the following tips from my FAC mentor, Noga Inbar:
+- Intro to API's- https://zapier.com/learn/apis/chapter-1-introduction-to-apis/
+
+- General Assembly has a [free online resource](https://dash.generalassemb.ly/projects) which fun and builds basic things but gives you an idea of how html, css, and js interact (note though that they use jquery)
+
+- Comprehensive book on DOM manipulation by Jeremy Keith called [DOM scripting](http://solmazakademi.com/media/academy_pdf/DOM_Scripting_Web_Design_with_JavaScript_and_the_Document_Object_Model_In1UMhl.pdf)
--- resources/source-files/script-copy.js
+++ resources/source-files/script-copy.js
@@ -1,4 +1,12 @@
-  var marker;
+//Following is the main google maps API function. 
+//This part doesn't need any updating to JS. For refactoring, scroll down to 
+//the reviewToggle function
+//Functionality to add:
+//*Review to become 'current' on clicking the relevant map marker
+//*Map markers to bounce as you scroll
+//*Site to be responsive
+
+var marker;
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -103,7 +111,6 @@ function initMap() {
     new google.maps.event.addListener(markerEspress, 'click', function(e) {toggleBounce(markerEspress)});
     new google.maps.event.addListener(markerAdolfo, 'click', function(e) {toggleBounce(markerAdolfo)});
 
-
   function toggleBounce(marker) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function() { 
@@ -147,8 +154,13 @@ bounceCurrent(markerEspress,espress);
 bounceCurrent(markerAdolfo,adolfo);
 
 }
+//I believe that through to here is all vanilla js, so this 
+//should be fine to leave as it is in the refactoring work.
 
+//From here on down is the jquery
 
+//The following function is the scrolling part that 
+//shows each review as you scroll down the list
 var reviewToggle = function() {
   $('.review-wrapper').click(function() {
     $('.review-wrapper').removeClass('current');
@@ -156,11 +168,15 @@ var reviewToggle = function() {
       $(this).addClass('current');
       $(this).children('.review-container').show(); 
   })
-
+ 
+ //The following variables ensure that you can keep scrolling down or up
+ //So the scroll returns to the top/bottom of the list when you reach the 
+ //bottom/top of the list, depending on the scroll direction.
   var firstReviewWrap = $('div.review-wrapper:first-of-type');
   var lastReviewWrap = $('div.review-wrapper:last-of-type');
   var currentListing = $('.current');
 
+//This sets the current class to the first review on document loading
   $(document).ready(function() {
     var currentListing = $('.current');
     var nextListing = currentListing.next();
@@ -168,6 +184,8 @@ var reviewToggle = function() {
     firstReviewWrap.children('.review-container').show();
   });
 
+//This is a one-off event to enable scrolling from the first element on load.
+//It's necessary because of the way I constructed the rest of the code.
   $(document).one('bind','keydown',function(evt) {
     if(event.which === 40) {
         firstReviewWrap.removeClass('current');
@@ -178,6 +196,8 @@ var reviewToggle = function() {
       }
   });
 
+//This is the main scroll function. The key code for the down arrow is 40
+//The key code for the up arrow is 38.
   $(document).bind('keydown',function(evt) {
        if(event.which === 40) {
           var currentListing = $('.current');
